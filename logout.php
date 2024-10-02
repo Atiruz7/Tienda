0d66837d4366f405ea4e12f9e2bf017a0fc27499
Atiruz7 <159601690+Atiruz7@users.noreply.github.com>
2024-10-02 16:24:43 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,14 @@
 <?php
-session_start();
-session_destroy();
-header("Location: login.php");
-exit();
+/**
+ * Cierra la sesión del usuario.
+ *
+ * Este script finaliza la sesión del usuario actual
+ * y redirige al usuario a la página de inicio de sesión.
+ *
+ * @return void No retorna ningún valor.
+ */
+session_start(); //inicia la sesion
+session_destroy(); //destruye los datos de la sesion
+header("Location: login.php");//envia al usuario a la pagina de inicio de sesion
+exit(); //finaliza el script
 ?>
